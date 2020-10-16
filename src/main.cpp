60f7f9e878852486530684b5c4255234bae25ce4
Espen <EspenBG>
2020-10-16 17:07:03 +0200

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <Arduino.h>
+/*  This is the main program for testing and development of an arduino client used
+ *  for gathering and sending information to a server
+ *
+ */
+
+void setup() {
+// write your initialization code here
+}
+
+void loop() {
+// write your code here
+}
